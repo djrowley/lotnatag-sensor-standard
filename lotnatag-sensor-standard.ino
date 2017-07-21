28ccc79b4045fb98ae3bf931bfba4775666bb8da
D Rowley <djrowley@users.noreply.github.com>
2017-07-21 19:33:22 +0100

    Added basic shields #9

--- lotnatag-sensor-standard.ino
+++ lotnatag-sensor-standard.ino
@@ -45,32 +45,48 @@ const byte hitDetectR = 255;
 const byte hitDetectG = 0;
 const byte hitDetectB = 0;
 
+const byte hitDetectShieldsR = 0;
+const byte hitDetectShieldsG = 0;
+const byte hitDetectShieldsB = 255;
+
 const byte reportHpR = 255;
 const byte reportHpG = 0;
 const byte reportHpB = 0;
 
-const byte regenR = 0;
-const byte regenG = 255;
-const byte regenB = 0;
-
+const byte reportHpShieldsR = 0;
+const byte reportHpShieldsG = 0;
+const byte reportHpShieldsB = 255;
 
 const byte startupR = 255;
 const byte startupG = 255;
 const byte startupB = 255;
 
+const byte regenR = 0;
+const byte regenG = 255;
+const byte regenB = 0;
+
+const byte rechargeR = 0;
+const byte rechargeG = 0;
+const byte rechargeB = 255;
+
 
 // SHIELD SETTINGS
 
-// todo - are shields enabled
-// todo - shield hit points
-// todo - shield recovery rate (0 for full shields instantly)
+bool shieldsEnabled = true;
+
+const unsigned int shieldHitpoints = 3;           // how many hitpoints does the shield hold
+const unsigned int shieldHealFrequencySecs = 3;   // how rapidly should regeneration happen (every X seconds)
+const unsigned int shieldRecoveryPeriodSecs = 10; // how many seconds need to elapse before regeneration can start
+const unsigned int shieldHealAmount = 1;          // how many hp are regenerated on each regen pulse (0 for 'to full')
 
 
 // REGENERATION SETTINGS
 
 bool regenerationEnabled = true;
+
 const unsigned int regenerationHealFrequencySecs = 3;   // how rapidly should regeneration happen (every X seconds)
 const unsigned int regenerationRecoveryPeriodSecs = 10; // how many seconds need to elapse before regeneration can start
+const unsigned int regenerationHealAmount = 1;          // how many hp are regenerated on each regen pulse (0 for 'to full')
 
 
 
@@ -125,11 +141,13 @@ const int mode_address = 16;
 // current settings of device
 byte current_max_hits = 0;
 byte current_hits = 0;
+byte current_shield_hits = 0;
 bool npcMode = false;
 
 // used for clock tracking of events
 unsigned long systemClockSeconds = 0;
 unsigned long time_lastShotReceived = 0;
+unsigned long time_lastShotReceivedOnShields = 0;
 unsigned long time_enteredDyingState = 0;
 
 // used during startup config period
@@ -326,15 +344,9 @@ void loop() {
 void shotDetected()
 {
     Serial.println("** HIT RECEIVED **");
-    
-    time_lastShotReceived = systemClockSeconds;
+
     unsigned long internalGracePeriodClock = millis();
-
-    ledOn(hitDetectR,hitDetectG,hitDetectB);
-    playSensorHit();
-
-    ledOff();
-    
+   
     bool stillAlive = removeHitpoint();
 
     if(stillAlive)
@@ -356,12 +368,35 @@ void shotDetected()
 // removes a hitpoint from the player, returns true if still alive
 bool removeHitpoint()
 {
-  if(current_hits > 0)
+  if(shieldsEnabled && current_shield_hits > 0)
+  {
+    current_shield_hits--;
+
+    time_lastShotReceivedOnShields = systemClockSeconds;
+
+    logMessage = "Shields remaining: ";
+    logMessage = logMessage + String(current_shield_hits);
+    Serial.println(logMessage);
+
+    ledOn(hitDetectShieldsR,hitDetectShieldsG,hitDetectShieldsB);
+    playSensorHitOnShields();
+    ledOff();
+  }
+  
+  else if(current_hits > 0)
+  {
     current_hits --;
+
+    time_lastShotReceived = systemClockSeconds;
     
-  logMessage = "Hits remaining: ";
-  logMessage = logMessage + String(current_hits);
-  Serial.println(logMessage);
+    logMessage = "Hits remaining: ";
+    logMessage = logMessage + String(current_hits);
+    Serial.println(logMessage);
+
+    ledOn(hitDetectR,hitDetectG,hitDetectB);
+    playSensorHit();
+    ledOff();
+  }
 
    if(current_hits <= 0)
    {
@@ -389,6 +424,19 @@ bool addHitpoint()
     
   logMessage = "Hits remaining: ";
   logMessage = logMessage + String(current_hits);
+  Serial.println(logMessage);
+  
+  return true;
+}
+
+// adds a hit point to the player's shields (i.e. recharge)
+bool addShieldHitpoint()
+{
+  if(current_shield_hits < shieldHitpoints)
+    current_shield_hits ++;
+    
+  logMessage = "Shield Hits remaining: ";
+  logMessage = logMessage + String(current_shield_hits);
   Serial.println(logMessage);
   
   return true;
@@ -491,8 +539,8 @@ void doTimerAction()
     case STATE_STARTUP:
           break;
     case STATE_ACTIVE:
-          // TODO - if shielded
-
+          if(shieldsEnabled)
+            rechargeShields();
 
           if(regenerationEnabled)
             regenerate();
@@ -565,6 +613,45 @@ void regenerate()
           }
       }           
     }
+}
+
+void rechargeShields()
+{
+    //check regeneration enabled
+    if(!shieldsEnabled)
+      return; //nope
+
+    // if injured
+    if(current_shield_hits < shieldHitpoints)
+    {
+      // how long until we start
+      int timeUntilRecharge = time_lastShotReceivedOnShields + shieldRecoveryPeriodSecs - systemClockSeconds; 
+      // how long have we been healing
+      
+      if(timeUntilRecharge > 0)
+      {
+          logMessage = "Shield Recharge begins in: ";
+          logMessage = logMessage + timeUntilRecharge;
+          Serial.println(logMessage);
+      }
+      else 
+      {
+          // how long have we been healing
+          int timeSinceRechargeStarted = systemClockSeconds - (time_lastShotReceivedOnShields + shieldRecoveryPeriodSecs);
+          // regenerate every X seconds
+          if (timeSinceRechargeStarted % shieldHealFrequencySecs == 0)
+          {
+            addShieldHitpoint();
+            ledOn(rechargeR,rechargeG,rechargeB);
+            playShieldRechargeReceived();
+            ledOff();
+          }
+      }           
+    }
+
+    delay(100);
+  
+    
 }
 
 // player has bled out and is now DEAD.
@@ -747,9 +834,35 @@ void playSensorHit()
   }
 }
 
+void playSensorHitOnShields()
+{
+  for(int i =0; i<51;i+=5){
+    tone(buzzerPin,200+(20*i),18);
+    delay(22);
+  }
+  for(int i =0; i<51;i+=5){
+    tone(buzzerPin,200+(20*i),18);
+    delay(22);
+  }
+  for(int i =0; i<51;i+=5){
+    tone(buzzerPin,200+(20*i),18);
+    delay(22);
+  }
+}
+
 void playHealReceived()
 {
-    for(int i =0; i<51;i+=2){
+    for(int i =0; i<51;i+=3){
+      tone(buzzerPin,500+(50*i),18);
+      delay(22);
+  }
+}
+
+void playShieldRechargeReceived()
+{
+    for(int i =0; i<51;i+=6){
+      tone(buzzerPin,200+(50*i),18);
+      delay(22);
       tone(buzzerPin,500+(50*i),18);
       delay(22);
   }
@@ -895,5 +1008,6 @@ void ledOff()
     digitalWrite(oneLedPin, LOW);
   }
 }
+
 
 
